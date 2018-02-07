0e4b22dc43eef9e93446f5d6d08534d6fc43a630
whsasf <whsasf@126.com>
2018-02-07 22:26:07 +0800

    new

--- lib/expect.sh
+++ lib/expect.sh
@@ -3,26 +3,18 @@
 # this tool makes ssh automatically via expect tool. so expect must be installed on the hosts requests from .
 
 expect <<-EOF
-
-# record logs file
 log_file expect.log  
-
 set timeout $expect_timeout
-
 spawn ssh $expect_user@${MX1_HOST_IP1}
-
-# enter password
-expect {  
- "*yes/no"   { send "yes\r"; exp_continue }
- "*Password:" { send "$expect_pass\r" }
-       }
-#switch to target account
-expect  "*#" { send "su - "$argv 1" \r" }       
-
-#do all operations
-for (( i=2;i<=$#;i++))
-do       
-expect  "*$" { send "$argv 2\r" }  
-done
-exit
+expect {
+ "*yes/no"   {send "yes\r"; exp_continue}
+ "*Password:" {send "$expect_pass\r"}
+}
+expect "~*#" {send "su - $1\r"}
+expect "~" {send "$2\r"}
+expect "~" {send "date -s\r"}
+expect "~" {send "ifconfig\r"}
+expect "~" {send "exit\r"}
 EOF
+
+cal
